9a3707c63082b5d43ad4182b9539d299a7899dc3
lsh <liu601545126@qq.com>
2017-12-15 13:42:26 +0800

    【add QRCODE】 内容：add QRCODE

--- client/PentaKillClient/app/src/main/java/com/sf/oarage/pentakillclient/qrcode/QrCodeActivity.java
+++ client/PentaKillClient/app/src/main/java/com/sf/oarage/pentakillclient/qrcode/QrCodeActivity.java
@@ -1,17 +1,42 @@
 package com.sf.oarage.pentakillclient.qrcode;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.sf.oarage.pentakillclient.R;
+import com.sf.oarage.pentakillclient.utils.QrCodeUtil;
 
 /**
  * Created by liushihan on 2017/12/15.
  */
 
 public class QrCodeActivity extends AppCompatActivity {
+    ImageView imageQrCode;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrcode_for_wechat);
+        imageQrCode = findViewById(R.id.image_qrcode);
+        imageQrCode.setImageBitmap(QrCodeUtil.createQRCodeWithLogo6("http://www.jianshu.com/users/4a4eb4feee62/latest_articles", 500, drawableToBitmap(getResources().getDrawable(R.mipmap.qrcode_logo))));
+    }
+
+    public static Bitmap drawableToBitmap(Drawable drawable) {
+        if (drawable instanceof BitmapDrawable) {
+            return ((BitmapDrawable) drawable).getBitmap();
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+
+        return bitmap;
     }
 }
