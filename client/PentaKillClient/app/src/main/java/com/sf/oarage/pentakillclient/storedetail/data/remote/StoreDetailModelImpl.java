c3f28702415dcf5ddd1e2107753a5bb6e2628625
陈碧云(Cathy Chen)-顺丰科技 <chenbiyun@sf-express.com>
2017-12-15 16:45:32 +0800

    接口调通

--- client/PentaKillClient/app/src/main/java/com/sf/oarage/pentakillclient/storedetail/data/remote/StoreDetailModelImpl.java
+++ client/PentaKillClient/app/src/main/java/com/sf/oarage/pentakillclient/storedetail/data/remote/StoreDetailModelImpl.java
@@ -2,6 +2,7 @@ package com.sf.oarage.pentakillclient.storedetail.data.remote;
 
 import com.sf.oarage.pentakillclient.Constants;
 import com.sf.oarage.pentakillclient.network.Callback;
+import com.sf.oarage.pentakillclient.network.RestGetRequester;
 import com.sf.oarage.pentakillclient.network.RestPostRequester;
 
 /**
@@ -11,14 +12,14 @@ import com.sf.oarage.pentakillclient.network.RestPostRequester;
 public class StoreDetailModelImpl implements StoreDetailModel {
     @Override
     public void reqStoreDetail(String storeId, Callback callback) {
-        RestPostRequester requester = new RestPostRequester(Constants.Method.STORE_DETAIL);
-        requester.execute(storeId, callback);
+        RestGetRequester requester = new RestGetRequester(Constants.Method.STORE_DETAIL+"/"+storeId);
+        requester.execute(callback);
     }
 
     @Override
     public void reqMarket(String marketId, Callback callback) {
-        RestPostRequester requester = new RestPostRequester(Constants.Method.MARKET_DATA);
-        requester.execute(marketId, callback);
+        RestGetRequester requester = new RestGetRequester(Constants.Method.MARKET_DATA+"/"+marketId);
+        requester.execute(callback);
     }
 
 }
