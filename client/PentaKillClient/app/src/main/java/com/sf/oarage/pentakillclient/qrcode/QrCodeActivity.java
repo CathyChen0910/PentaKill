2345ca6fb865db472eb351ca2cbf5bad1cace7db
lsh <liu601545126@qq.com>
2017-12-15 12:46:11 +0800

    【add QRCODE】 内容：add QRCODE

---
+++ client/PentaKillClient/app/src/main/java/com/sf/oarage/pentakillclient/qrcode/QrCodeActivity.java
@@ -0,0 +1,17 @@
+package com.sf.oarage.pentakillclient.qrcode;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by liushihan on 2017/12/15.
+ */
+
+public class QrCodeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrcode_for_wechat);
+    }
+}
