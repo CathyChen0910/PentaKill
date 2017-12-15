eb6d910eed6e2bff0b5290dc2b1a8ffc7aca6f00
wo <651328352@qq.com>
2017-12-15 16:00:14 +0800

    update

--- server/src/main/java/com/sf/marathon/pentakill/server/task/GenerateGroupTask.java
+++ server/src/main/java/com/sf/marathon/pentakill/server/task/GenerateGroupTask.java
@@ -2,29 +2,32 @@ package com.sf.marathon.pentakill.server.task;
 
 import java.util.concurrent.Future;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Component;
 
 import com.sf.marathon.pentakill.server.service.IGroupService;
+import com.sf.marathon.pentakill.server.util.LockUtil;
 
 @Component
 public class GenerateGroupTask {
 
-	private static final Log log = LogFactory.getLog(GenerateGroupTask.class);
+	private static final String PREFIX = "_generategroup_";
 
 	@Autowired
 	private IGroupService groupService;
 
 	@Async("mySimpleAsync")
 	public Future<Boolean> generate(String marketId) {
-		// lock marketId
-		groupService.generate(marketId);
-		// unlock
-		return new AsyncResult<>(true);
+		String key = PREFIX + marketId;
+		LockUtil.lock(key);
+		try {
+			groupService.generate(key);
+			return new AsyncResult<>(true);
+		} finally {
+			LockUtil.unLock(key);
+		}
 	}
 
 }
