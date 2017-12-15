f07064a528895278ce49a7e6045df459229b835f
spawn <aabbcclcl@hotmail.com>
2017-12-15 15:03:01 +0800

    日历选择控件

--- client/PentaKillClient/app/src/main/java/com/sf/oarage/pentakillclient/editsendinfo/EditSendInfoActivity.java
+++ client/PentaKillClient/app/src/main/java/com/sf/oarage/pentakillclient/editsendinfo/EditSendInfoActivity.java
@@ -11,6 +11,14 @@ import android.widget.Toast;
 
 import com.sf.oarage.pentakillclient.R;
 
+import chihane.jdaddressselector.AddressSelector;
+import chihane.jdaddressselector.BottomDialog;
+import chihane.jdaddressselector.OnAddressSelectedListener;
+import chihane.jdaddressselector.model.City;
+import chihane.jdaddressselector.model.County;
+import chihane.jdaddressselector.model.Province;
+import chihane.jdaddressselector.model.Street;
+
 /**
  * Created by liushihan on 2017/12/15.
  */
@@ -36,6 +44,24 @@ public class EditSendInfoActivity extends AppCompatActivity implements EditSendI
         editSendCount = findViewById(R.id.edit_send_count);
         editWeight = findViewById(R.id.edit_single_weight);
         btnSign = findViewById(R.id.btn_sign);
+        setListener();
+    }
+
+    private void setListener() {
+        textAddressOfPicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BottomDialog dialog = new BottomDialog(EditSendInfoActivity.this);
+                dialog.setOnAddressSelectedListener(new OnAddressSelectedListener() {
+                    @Override
+                    public void onAddressSelected(Province province, City city, County county, Street street) {
+                        //判空
+
+                    }
+                });
+                dialog.show();
+            }
+        });
     }
 
     @Override
@@ -45,14 +71,14 @@ public class EditSendInfoActivity extends AppCompatActivity implements EditSendI
 
     @Override
     public void showToast(String message) {
-        Toast.makeText(EditSendInfoActivity.this,"",Toast.LENGTH_SHORT).show();
+        Toast.makeText(EditSendInfoActivity.this, "", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.btn_sign:
-                if (mPresenter.checkRequiredFill()){
+                if (mPresenter.checkRequiredFill()) {
                     mPresenter.signOfStore();
                 }
                 break;
