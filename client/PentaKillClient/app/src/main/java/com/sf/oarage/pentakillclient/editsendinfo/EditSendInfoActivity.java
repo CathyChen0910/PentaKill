8467701a89e16e6b85665a5064c34dabe3ba6bcc
lsh <liu601545126@qq.com>
2017-12-15 14:35:33 +0800

    【add QRCODE】 内容：add QRCODE

--- client/PentaKillClient/app/src/main/java/com/sf/oarage/pentakillclient/editsendinfo/EditSendInfoActivity.java
+++ client/PentaKillClient/app/src/main/java/com/sf/oarage/pentakillclient/editsendinfo/EditSendInfoActivity.java
@@ -3,6 +3,9 @@ package com.sf.oarage.pentakillclient.editsendinfo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.sf.oarage.pentakillclient.R;
 
@@ -11,10 +14,22 @@ import com.sf.oarage.pentakillclient.R;
  */
 
 public class EditSendInfoActivity extends AppCompatActivity{
+    TextView textAddressOfPicker;
+    EditText editAddressDetail;
+    EditText editName;
+    EditText editPhone;
+    EditText editSendCount;
+    EditText editWeight;
+    Button btnSign;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_sendinfo);
-
+        textAddressOfPicker = findViewById(R.id.textAddress);
+        editAddressDetail = findViewById(R.id.textAddressDetail);
+        editName = findViewById(R.id.edit_sender_name);
+        editPhone = findViewById(R.id.edit_send_count);
+        editWeight = findViewById(R.id.edit_single_weight);
+        btnSign = findViewById(R.id.btn_sign);
     }
 }
