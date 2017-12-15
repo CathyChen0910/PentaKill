bedc456db47429e20d7cd7dede114964e1c3874a
陈碧云(Cathy Chen)-顺丰科技 <chenbiyun@sf-express.com>
2017-12-15 13:22:39 +0800

    配置

---
+++ client/PentaKillClient/app/src/main/java/com/sf/oarage/pentakillclient/storelist/StoreListContract.java
@@ -0,0 +1,20 @@
+package com.sf.oarage.pentakillclient.storelist;
+
+import com.sf.oarage.pentakillclient.base.BasePresenter;
+import com.sf.oarage.pentakillclient.base.BaseView;
+
+/**
+ * Created by Cathy on 2017/12/15.
+ */
+
+public interface StoreListContract {
+
+    interface StoreListPresenter extends BasePresenter{
+
+    }
+
+    interface StoreListView extends BaseView<StoreListPresenter>{
+
+    }
+
+}
